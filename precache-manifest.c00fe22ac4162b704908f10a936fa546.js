e4f25e7706ca784fa55c43f4dd2452d328265ce1
KJ <awedsheep@gmail.com>
2020-02-04 23:25:30 -0600

    Updates

---
+++ precache-manifest.c00fe22ac4162b704908f10a936fa546.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "511ee147a3acfcaf2579e33c5d84355b",
+    "url": "/weko/index.html"
+  },
+  {
+    "revision": "d137b8a113a96d8b364d",
+    "url": "/weko/static/css/2.9887b5d1.chunk.css"
+  },
+  {
+    "revision": "9bf1574bee1d9b3da13a",
+    "url": "/weko/static/css/main.f328e6c8.chunk.css"
+  },
+  {
+    "revision": "d137b8a113a96d8b364d",
+    "url": "/weko/static/js/2.113b4ad5.chunk.js"
+  },
+  {
+    "revision": "5ce9103cb303ebfd78e368699d460b5a",
+    "url": "/weko/static/js/2.113b4ad5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bf1574bee1d9b3da13a",
+    "url": "/weko/static/js/main.138cfcd5.chunk.js"
+  },
+  {
+    "revision": "c7ee58ad4f88382b72ee",
+    "url": "/weko/static/js/runtime-main.2ad88260.js"
+  },
+  {
+    "revision": "2802419045a805c209164e24033bf02d",
+    "url": "/weko/static/media/avatar.28024190.png"
+  },
+  {
+    "revision": "f6627058f7e13aa6d8c859232019faa9",
+    "url": "/weko/static/media/banner0.f6627058.jpg"
+  },
+  {
+    "revision": "3ae4612bd273a1940070d074aa8483d1",
+    "url": "/weko/static/media/banner1.3ae4612b.jpg"
+  },
+  {
+    "revision": "b29cc619bfb1dc2c2c9c2f6efbdae51c",
+    "url": "/weko/static/media/banner2.b29cc619.jpg"
+  },
+  {
+    "revision": "5a9cb819a1bc78f553a0369e976e7c3c",
+    "url": "/weko/static/media/banner3.5a9cb819.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/weko/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/weko/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/weko/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/weko/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/weko/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/weko/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/weko/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/weko/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/weko/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/weko/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/weko/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "843693ba7965e80ec04f77056ece3936",
+    "url": "/weko/static/media/logo.843693ba.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/weko/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/weko/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/weko/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/weko/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/weko/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "03f45d882ff3a80b719cfc0789c5eab9",
+    "url": "/weko/static/media/summernote.03f45d88.woff"
+  },
+  {
+    "revision": "549304631a5f1ab69498e294543086e3",
+    "url": "/weko/static/media/summernote.54930463.ttf"
+  },
+  {
+    "revision": "7e263200d01fb5998c401b03ef3e6433",
+    "url": "/weko/static/media/summernote.7e263200.eot"
+  }
+]);
